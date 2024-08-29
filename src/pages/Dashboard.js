3ea3815d6d31aaee6d1bb8541778c3f5ffa5940b
PRIYANSHU SONI <122470603+priyanshu08soni@users.noreply.github.com>
2024-08-29 20:53:11 +0530

    success  in data extraction

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -5,7 +5,7 @@ import Details from "../components/Details";
 import Overview from "../components/Overview";
 import ThemeContext from "../context/ThemeContext";
 import StockContext from "../context/StockContext";
-import { MdContentPasteSearch, MdWidthFull } from "react-icons/md";
+import { MdContentPasteSearch } from "react-icons/md";
 import { BiAnalyse } from "react-icons/bi";
 import { LiaOpencart } from "react-icons/lia";
 import StockDetails from "../components/StockDetails";
@@ -16,36 +16,84 @@ const Dashboard = () => {
   const { darkMode } = useContext(ThemeContext);
   const { stockSymbol } = useContext(StockContext);
   const [allStocks, setAllStocks] = useState(mockSearchResults.result);
-  const [stockData,setStockData] = useState(historicalData);
   const [stockDetails, setStockDetails] = useState({
     Company: "Company",
     ISIN: "ISIN",
     Industry: "Industry",
-    Series: "Symbol",
+    Series: "Series",
     Symbol: "Symbol",
   });
   const [fromDate,setFromDate] = useState("2020-05-04");
   const [toDate,setToDate] = useState("2021-04-30");
+  const [high,setHigh] = useState(-Infinity);
+  const [low,setLow] = useState(Infinity);
+  const [volume,setVolume] =useState(0);
+  // console.log(historicalData[stockSymbol]);
+  // useEffect(()=>{
+  //   let tempHigh = -Infinity;
+  //   let tempLow = Infinity;
+  //   let cumulativeVolume =  0;
+  //   let currentDate = new Date(fromDate);
+  //   let endDate = new Date(toDate);
+  //   for(let i = 0;i<stockData?.length;i++){
+  //     if(historicalData[stockSymbol] && fromDate === historicalData[stockSymbol][i]?.Date){
+  //       while(currentDate < endDate && i<250){
+  //         if(high < historicalData[stockSymbol][i]?.High){
+  //           tempHigh = Math.max(tempHigh,historicalData[stockSymbol][i].High);
+  //         }
+  //         if(low > historicalData[stockSymbol][i]?.Low){
+  //           tempLow = Math.min(tempLow,historicalData[stockSymbol][i].Low);
+  //         }
+  //         cumulativeVolume = cumulativeVolume+historicalData[stockSymbol][i].Volume;
+  //         currentDate.setDate(currentDate.getDate()+1);
+  //       }
+  //       break;
+  //     }
+  //   }
+  //   setHigh(tempHigh);
+  //   setLow(tempLow);
+  //   setVolume(cumulativeVolume);
+  // },[toDate,fromDate,stockSymbol]);
 
-  console.log(stockData)
-
+  const [stockData,setStockData] = useState([]);
   useEffect(()=>{
     let data = [];
     let currentDate = new Date(fromDate);
     let endDate = new Date(toDate);
-    for(let i = 0;i<250;i++){
-      if(historicalData[stockSymbol] && fromDate === historicalData[stockSymbol][i]?.Date){
-        while(currentDate <= endDate && i<250){
-          data.push(historicalData[stockSymbol][i++]);
-          currentDate.setDate(currentDate.getDate()+1);
+    
+    for(let i = 0;i<historicalData[stockSymbol]?.length;i++){
+      let date =new Date(historicalData[stockSymbol][i]?.Date);
+      let month =new Date(historicalData[stockSymbol][i]?.Date);
+      let year =new Date(historicalData[stockSymbol][i]?.Date);
+      if( 
+        currentDate.getDate() <= date?.getDate() &&
+        currentDate.getMonth() <= month.getMonth() &&
+        currentDate.getFullYear() <= year.getFullYear()
+      ){
+        while(currentDate <= endDate){
+          let date =new Date(historicalData[stockSymbol][i]?.Date);
+          let month =new Date(historicalData[stockSymbol][i]?.Date);
+          let year =new Date(historicalData[stockSymbol][i]?.Date);
+          if( 
+            currentDate.getDate() === date?.getDate() &&
+            currentDate.getMonth() === month.getMonth() &&
+            currentDate.getFullYear() === year.getFullYear() 
+          ){
+            data.push(historicalData[stockSymbol][i++]);
+            currentDate.setDate(currentDate.getDate()+1);
+          }else{
+            currentDate.setDate(currentDate.getDate()+1);
+          }
         }
+        break;
       }
     }
     setStockData(data);
-  },[fromDate,toDate])
+    console.log(stockData,currentDate.getDate(),currentDate.getMonth()+1,currentDate.getFullYear(),endDate.getDate(),endDate.getMonth()+1,endDate.getFullYear());
+  },[fromDate,toDate,stockSymbol])
   useEffect(() => {
     for (let i = 0; i < allStocks.length; i++) {
-      if (stockSymbol == allStocks[i].Symbol) {
+      if (stockSymbol === allStocks[i].Symbol) {
         setStockDetails({
           Company: allStocks[i]["Company Name"],
           ISIN: allStocks[i]["ISIN Code"],
@@ -74,38 +122,38 @@ const Dashboard = () => {
         </div>
         <div className='flex justify-end gap-4'>
             <input type="date" value={fromDate} onChange={e=>setFromDate(e.target.value)} max="2021-04-29" min="2020-05-04" className='my-2 py-1 px-4 rounded-md bg-blue-950 border-2 border-gray-600' />
-            <input type="date" value={toDate} onChange={e=>setToDate(e.target.value)} max="2021-04-30" min="2020-05-05" className='my-2 py-1 px-4 rounded-md bg-blue-950 border-2 border-gray-600' />
+            <input type="date" value={toDate} onChange={e=>setToDate(e.target.value)} min="2020-05-05" max="2021-04-30"  className='my-2 py-1 px-4 rounded-md bg-blue-950 border-2 border-gray-600' />
         </div>
         <div className='flex flex-wrap justify-between'>
             <div className={`h-full card rounded-md relative p-8 border-2 bg-gray-300 shadow-md my-3 ${darkMode?"bg-gray-900 border-gray-800":"bg-white border-blue-100"}`}
             style={{ color:"gray",overflow:"auto"}}
             >
                 <div className='font-extrabold text-xl pb-3' >P. CLOSE</div>
-                <div className='font-bold text-blue-300'>9,914.20</div>
+                <div className='font-bold text-blue-300'>{stockData[0]? stockData[0]["Prev Close"]:""}</div>
             </div>
             <div className={`h-full card rounded-md relative p-8 border-2 bg-gray-300 shadow-md my-3 ${darkMode?"bg-gray-900 border-gray-800":"bg-white border-blue-100"}`}
             style={{ color:"gray",overflow:"auto"}}
             >
                 <div className='font-extrabold text-xl pb-3' >OPEN</div>
-                <div className='font-bold  text-blue-300'>9,970.00</div>
+                <div className='font-bold  text-blue-300'>{stockData[0]?stockData[0].Open:""}</div>
             </div>
             <div className={`h-full card rounded-md relative p-8 border-2 bg-gray-300 shadow-md my-3 ${darkMode?"bg-gray-900 border-gray-800":"bg-white border-blue-100"}`}
             style={{ color:"gray",overflow:"auto"}}
             >
                 <div className='font-extrabold text-xl pb-3' >HIGH</div>
-                <div className='font-bold text-blue-300'>10,444.35</div>
+                <div className='font-bold text-blue-300'>{high?high:""}</div>
             </div>
             <div className={`h-full card rounded-md relative p-8 border-2 bg-gray-300 shadow-md my-3 ${darkMode?"bg-gray-900 border-gray-800":"bg-white border-blue-100"}`}
             style={{ color:"gray",overflow:"auto"}}
             >
                 <div className='font-extrabold text-xl pb-3' >LOW</div>
-                <div className='font-bold  text-blue-300'>9,930.00</div>
+                <div className='font-bold  text-blue-300'>{low?low:""}</div>
             </div>
             <div className={`h-full card rounded-md relative p-8 border-2 bg-gray-300 shadow-md my-3 ${darkMode?"bg-gray-900 border-gray-800":"bg-white border-blue-100"}`}
             style={{ color:"gray",overflow:"auto"}}
             >
                 <div className='font-extrabold text-xl pb-3' >CLOSE*</div>
-                <div className='font-bold  text-blue-300'>10,406.45</div>
+                <div className='font-bold  text-blue-300'>{stockData[stockData.length-1]?stockData[stockData.length-1].Close:""}</div>
             </div>
             <div className={`h-full card rounded-md relative p-8 border-2 bg-gray-300 shadow-md my-3 ${darkMode?"bg-gray-900 border-gray-800":"bg-white border-blue-100"}`}
             style={{ color:"gray",overflow:"auto"}}
