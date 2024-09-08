0e926853c67c49f31b4335b2b7e0f6554bb2ca9f
PRIYANSHU SONI <122470603+priyanshu08soni@users.noreply.github.com>
2024-09-08 20:25:38 +0530

    commit

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,13 @@
 import React, { useContext } from "react";
 import { MdOutlineLeaderboard } from "react-icons/md";
-import { NavLink } from "react-router-dom";
+import { NavLink, useLocation } from "react-router-dom";
 import ThemeIcon from "./ThemeIcon";
 import ThemeContext from "../context/ThemeContext";
 
 const Header = () => {
-  const { setDarkMode,darkMode } = useContext(ThemeContext);
-
+  const { setDarkMode, darkMode } = useContext(ThemeContext);
+  const location = useLocation();
+  const isDashboard = location.pathname === "/dashboard";
   function menuOnClick() {
     document.getElementById("menu-bar")?.classList.toggle("change");
     document.getElementById("nav")?.classList.toggle("change");
@@ -18,9 +19,24 @@ const Header = () => {
         <div className="pb-4">
           <div id="menu">
             <div id="menu-bar" onClick={menuOnClick}>
-              <div id="bar1" className={`bar ${darkMode ? "bg-blue-700 text-gray-300" : " bg-blue-700"} `}></div>
-              <div id="bar2" className={`bar ${darkMode ? "bg-blue-700 text-gray-300" : " bg-blue-700"} `}></div>
-              <div id="bar3" className={`bar ${darkMode ? "bg-blue-700 text-gray-300" : " bg-blue-700"} `}></div>
+              <div
+                id="bar1"
+                className={`bar ${
+                  darkMode ? "bg-blue-700 text-gray-300" : " bg-blue-700"
+                } `}
+              ></div>
+              <div
+                id="bar2"
+                className={`bar ${
+                  darkMode ? "bg-blue-700 text-gray-300" : " bg-blue-700"
+                } `}
+              ></div>
+              <div
+                id="bar3"
+                className={`bar ${
+                  darkMode ? "bg-blue-700 text-gray-300" : " bg-blue-700"
+                } `}
+              ></div>
             </div>
             <nav class="nav" id="nav">
               <ul>
@@ -29,8 +45,15 @@ const Header = () => {
                     Home
                   </NavLink>
                 </li>
-                <li onClick={()=>setDarkMode(true)}>
-                  <NavLink className="navlink"  to="/dashboard">
+                <li
+                  onClick={() => {
+                    if (!isDashboard) {
+                      setDarkMode(true);
+                    }
+                  }}
+                  className={isDashboard ? "nav-item disabled" : "nav-item"}
+                >
+                  <NavLink className="navlink" to="/dashboard">
                     Dashboard
                   </NavLink>
                 </li>
@@ -58,7 +81,7 @@ const Header = () => {
           <div class="menu-bg" id="menu-bg"></div>
         </div>
         <div className={`z-10 websitename text-blue-700 `}>-MS-</div>
-        <div className={`logo flex items-center text-blue-700`} >
+        <div className={`logo flex items-center text-blue-700`}>
           <MdOutlineLeaderboard />
         </div>
       </header>
